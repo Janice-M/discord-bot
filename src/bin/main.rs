78b347bfea3b4b9a08672419e8d7f564fd89dc55
Collins Muriuki <murerwacollins@gmail.com>
2020-10-17 11:45:32 +0300

    Add util test commands

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use discord_bot::generate_random_name;
 
 use serenity::{
     async_trait,
@@ -16,17 +17,29 @@ impl EventHandler for Handler {
                 println!("Error sending message: {:?}", why);
             }
         }
-        if msg.content == "!slide" {
-            let dm = msg.author.dm(&ctx, |m| {
-                m.content("Hello!");
 
-                m
-            }).await;
+        if msg.content == "!alias" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, generate_random_name()).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+
+        if msg.content == "!slide" {
+            let dm = msg
+                .author
+                .dm(&ctx, |m| {
+                    m.content("Hello!");
+
+                    m
+                })
+                .await;
 
             if let Err(why) = dm {
                 println!("Error when direct messaging user: {:?}", why);
             }
         }
+        
+
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
@@ -36,8 +49,7 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
     let mut client = Client::new(&token)
         .event_handler(Handler)
